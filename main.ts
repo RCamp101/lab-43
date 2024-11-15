c910e640dcb39b2a778b496faca0659d2a1dc3e8
RCamp101 <r.camp@mcaabhm.org>
2024-11-15 11:44:00 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+function findSuit (cardNumber: number) {
+    if (Math.idiv(cardNumber, 13) == 0) {
+        return "Spades"
+    } else if (Math.idiv(cardNumber, 13) == 1) {
+        return "Diamonds"
+    } else if (Math.idiv(cardNumber, 13) == 2) {
+        return "Clubs"
+    } else if (Math.idiv(cardNumber, 13) == 3) {
+        return "Hearts"
+    }
+}
+function findPip (cardNumber: number) {
+    if (cardNumber % 13 == 0) {
+        return "Ace"
+    } else if (cardNumber % 13 == 10) {
+        return "Jack"
+    } else if (cardNumber % 13 == 11) {
+        return "Queen"
+    } else if (cardNumber % 13 == 12) {
+        return "King"
+    } else {
+        return cardNumber % 13
+    }
+}
+let cardNumber = game.askForNumber("0-51")
+game.splash("" + cardNumber + " = " + findPip(cardNumber) + " of " + findSuit(cardNumber))
